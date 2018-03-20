5c56b55bfd9b504cec7b7fcc241944c95f187453
Christiebelle <cslester@outlook.com>
2018-03-20 16:24:59 +0000

    First run and successful output

--- src/main/java/controllers/EmployeesController.java
+++ src/main/java/controllers/EmployeesController.java
@@ -1,21 +1,22 @@
 package controllers;
 import db.DBHelper;
-import db.Seeds;
 import models.Employee;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
 import static spark.Spark.get;
-import static spark.SparkBase.staticFileLocation;
 
 public class EmployeesController {
 
-    public static void main(String[] args) {
-        staticFileLocation("/public");
-        ManagersController managersController = new ManagersController();
-        Seeds.seedData();
+    public EmployeesController(){
+        this.setupEndpoints();
+    }
+
+    private void setupEndpoints(){
 
         get("/employees", (req,res)->{
             Map<String, Object> model= new HashMap<>();
