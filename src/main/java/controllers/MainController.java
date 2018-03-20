65c8f7c50c2199ce9f8b87f408a348970ce8bbcc
Christiebelle <cslester@outlook.com>
2018-03-20 16:18:50 +0000

    Main Controller updated and homepage.vtl created and updated

--- src/main/java/controllers/MainController.java
+++ src/main/java/controllers/MainController.java
@@ -1,4 +1,26 @@
 package controllers;
 
+import static spark.Spark.get;
+import static spark.SparkBase.staticFileLocation;
+
+import db.Seeds;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainController {
+
+    public static void main(String[] args) {
+        Seeds.seedData();
+        staticFileLocation("/public");
+        EmployeesController employeesController = new EmployeesController();
+
+        get("/homepage", (req, res)->{
+            Map<String, Object> model = new HashMap<>();
+            model.put("template", "templates/homepage.vtl");
+            return new ModelAndView(model, "templates/layout.vtl");
+        }, new VelocityTemplateEngine());
+    }
 }
