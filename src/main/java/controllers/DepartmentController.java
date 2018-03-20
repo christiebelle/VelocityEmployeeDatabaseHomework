c1139020a8ef90da70e065a97b197fab05d80a11
Christiebelle <cslester@outlook.com>
2018-03-20 18:49:02 +0000

    Get/Post methods written for Department Class and create.vtl initialised

--- src/main/java/controllers/DepartmentController.java
+++ src/main/java/controllers/DepartmentController.java
@@ -2,6 +2,7 @@ package controllers;
 
 import db.DBHelper;
 import models.Department;
+import models.department;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 
@@ -10,6 +11,7 @@ import java.util.List;
 import java.util.Map;
 
 import static spark.Spark.get;
+import static spark.Spark.post;
 
 public class DepartmentController{
 
@@ -26,5 +28,23 @@ public class DepartmentController{
             model.put("template", "templates/departments/index.vtl");
             return new ModelAndView(model, "templates/layout.vtl");
         }, new VelocityTemplateEngine());
+
+        get("/departments/new", (req, res)->{
+            Map<String, Object> model = new HashMap<>();
+            List<Department> departments = DBHelper.getAll(Department.class);
+            model.put("departments", departments);
+            model.put("template", "templates/departments/create.vtl");
+            return new ModelAndView(model, "templates/layout.vtl");
+        }, new VelocityTemplateEngine());
+
+        post("/departments", (req, res)->{
+            String title = req.queryParams("title");
+            Department department = new Department(title);
+            DBHelper.save(department);
+            res.redirect("/departments");
+            return null;
+        }, new VelocityTemplateEngine());
     }
+    }
+
 }
